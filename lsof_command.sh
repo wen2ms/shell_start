acd3bbba25925c7dc7608f921aab8ed228e75c08
wen2ms <1819509111@qq.com>
2025-05-28 14:38:10 +0800

    Feat: lsof

---
+++ lsof_command.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# List Open Files
+lsof
+
+# -i: list network files
+lsof -i
+lsof -i:80
+lsof -i TCP:22
+
+# -u: by user
+lsof -u mysql
+
+# -p: by PID
+lsof -p 1234
+
+lsof | grep deleted
