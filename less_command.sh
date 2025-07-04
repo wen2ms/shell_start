c5f66bdda94e3c05e5cff93efa2810e5b9cfa7f5
wen2ms <1819509111@qq.com>
2025-07-04 17:35:00 +0800

    Feat: Less command

---
+++ less_command.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+less less_command.sh
+
+# -N: Show line number
+# -M: More details like percentage and total number of lines
+less -NM less_command.sh
+
+# Space: Next Page
+# b: Previous page
+
+# /pattern: Search
+# n: Next match
+# N: Previous match
+
+# g: jump to frist line
+# G: jump to last line
+
+# q: Quit
