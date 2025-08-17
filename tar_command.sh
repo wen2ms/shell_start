386bab9e3e4e03d09d646d51b5002e31270bd258
wen2ms <1819509111@qq.com>
2025-08-17 21:47:37 +0800

    Feat: tar command

---
+++ tar_command.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# tar: tape arhieve
+
+# compression
+
+# -c: create
+# -f: filename of archieve
+tar -cf foo.tar foo.sh foo.service
+
+# -z: gzip format
+tar -czf foo.tar.gz foo.sh foo.service
+
+# extract
+
+# -x: extract
+tar -x foo.tar
+
+# -C: change extracting directory
+tar -xzf foo.tar.gz -C ./
+
+# extract specific file from archieve
+tar -xf foo.tar foo.sh  
+
+# -t: list
+tar -tf foo.tar
+
+# -v: verbose in more detail than -t
+tar -tvf foo.tar
